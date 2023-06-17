816cb3e9e8cde34fde8d8f360925ffe5dcd76189
Ramazan Doğan <doganrmzn40@gmail.com>
2023-06-17 19:09:25 +0300

    Persist-Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,24 @@
+import { combineReducers } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
+import createWebStorage from 'redux-persist/es/storage/createWebStorage';
 import { movieApi } from '../services/movieApi';
 import moviesReducer from '../features/addMovie/moviesSlice';
+import persistReducer from 'redux-persist/es/persistReducer';
+const persistConfig = {
+   key: 'root',
+   version: 1,
+   storage: createWebStorage('local'), // veya AsyncStorage
+};
+
+const reducer = combineReducers({
+   [movieApi.reducerPath]: movieApi.reducer,
+   movies: moviesReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-   reducer: {
-      [movieApi.reducerPath]: movieApi.reducer,
-      movies: moviesReducer,
-   },
+   reducer: persistedReducer,
    middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(movieApi.middleware),
 });
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { persistStore } from 'redux-persist';
 import { store } from './app/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+let persistor = persistStore(store);
 
 root.render(
    <React.StrictMode>
       <Provider store={store}>
-         <App />
+         <PersistGate persistor={persistor}>
+            <App />
+         </PersistGate>
       </Provider>
    </React.StrictMode>
 );
